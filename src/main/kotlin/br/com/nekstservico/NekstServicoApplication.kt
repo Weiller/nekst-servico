cb0d20b5d9f87532dee2af4ed931a4b434d72376
Weiller Schepis <weillerschepis@gmail.com>
2019-06-24 20:12:22 -0300

    Firts commit

---
+++ src/main/kotlin/br/com/nekstservico/NekstServicoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.nekstservico
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NekstServicoApplication
+
+fun main(args: Array<String>) {
+	runApplication<NekstServicoApplication>(*args)
+}
