01f1e55860e9861d24cd5d0673a9fe0e1a3e1d5d
Weiller Schepis <weillerschepis@gmail.com>
2019-07-02 01:09:57 -0300

    integrando sonnar

--- src/test/kotlin/br/com/nekstservico/NekstServicoApplicationTests.kt
+++ src/test/kotlin/br/com/nekstservico/NekstServicoApplicationTests.kt
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
-
+@SpringBootTest
+@ContextConfiguration(classes = [EmptyDatabase::class])
 class NekstServicoApplicationTests {
 
+    @Test
     fun contextLoads() {
+        main(arrayOf())
+
     }
 }
